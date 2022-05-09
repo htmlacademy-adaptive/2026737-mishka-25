b784d3c71db8b6157fac2d92bd65850e6cdc025c
Комаров Сергей <serkomarov88@gmail.com>
2022-05-10 01:25:25 +0300

    Оптимизация графики страниц

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ import terser from 'gulp-terser';
 import squoosh from 'gulp-libsquoosh';
 import svgo from 'gulp-svgmin';
 import svgstore from 'gulp-svgstore';
+import del from 'del';
 
 // Styles
 
@@ -28,7 +29,7 @@ export const styles = () => {
 }
 
 //html
-export const html = () => {
+const html = () => {
   return gulp.src('source/*.html')
   .pipe(htmlmin({collapseWhitespace: true}))
   .pipe(gulp.dest('build'));
@@ -36,7 +37,7 @@ export const html = () => {
 
 // Scripts
 
-export const scripts = () => {
+const scripts = () => {
   return gulp.src('source/js/*.js')
   .pipe(terser())
   .pipe(gulp.dest('build/js'))
@@ -45,13 +46,13 @@ export const scripts = () => {
 
 // Images
 
-export const optimizeImages = () => {
+const optimizeImages = () => {
   return gulp.src('source/img/**/*.{png,jpg}')
   .pipe(squoosh())
   .pipe(gulp.dest('build/img'))
   }
 
-export const copyImages = () => {
+const copyImages = () => {
   return gulp.src('source/img/**/*.{png,jpg}')
   .pipe(gulp.dest('build/img'))
   }
@@ -68,13 +69,13 @@ const createWebp = () => {
 
 // SVG
 
-export const svg = () =>
-gulp.src('source/img/*.svg')
+const svg = () =>
+gulp.src(['source/img/*.svg', '!source/img/icons/*.svg'])
 .pipe(svgo())
 .pipe(gulp.dest('build/img'));
 
-export const sprite = () => {
-  return gulp.src('source/img/icons/*.svg')
+const sprite = () => {
+  return gulp.src('source/img/icon/*.svg')
   .pipe(svgo())
   .pipe(svgstore({
   inlineSvg: true
@@ -83,12 +84,32 @@ export const sprite = () => {
   .pipe(gulp.dest('build/img'));
   }
 
+// Copy
+
+const copy = (done) => {
+  gulp.src([
+  'source/fonts/*.{woff2,woff}',
+  'source/*.ico',
+  'source/manifest.webmanifest'
+  ], {
+  base: 'source'
+  })
+  .pipe(gulp.dest('build'))
+  done();
+  }
+
+// Clean
+
+const clean = () => {
+  return del('build');
+  };
+
 // Server
 
 const server = (done) => {
   browser.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
@@ -104,15 +125,39 @@ const watcher = () => {
   gulp.watch('source/*.html').on('change', browser.reload);
 }
 
+// Build
+
+export const build = gulp.series(
+  clean,
+  copy,
+  optimizeImages,
+  svg,
+  gulp.parallel(
+  styles,
+  html,
+  scripts,
+  // svg,
+  sprite,
+  createWebp
+  ),
+  );
+
+// Default
 
 export default gulp.series(
-  html,
-  styles,
-  scripts,
-  optimizeImages,
-  createWebp,
+  clean,
+  copy,
+  copyImages,
   svg,
+  gulp.parallel(
+  styles,
+  html,
+  scripts,
+  // svg,
   sprite,
+  createWebp
+  ),
+  gulp.series(
   server,
   watcher
-);
+  ));
