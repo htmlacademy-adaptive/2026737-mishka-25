c34da8afa37721cc5bd52c92da84a0d928c33f98
Комаров Сергей <serkomarov88@gmail.com>
2022-05-14 01:09:37 +0300

    Правки после проверки

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,9 @@
 let navMain = document.querySelector('.main-nav');
 let navToggle = document.querySelector('.main-nav__toggle');
+let navHeader = document.querySelector('.main-header');
 
 navMain.classList.remove('main-nav--nojs');
+navHeader.classList.remove('main-header--nojs');
 
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('main-nav--closed')) {
